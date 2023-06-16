34eddf1ecf5c6cc310a0a9cfa57d006807108c67
Nelson <adhikariramesh747@gmail.com>
2023-06-16 19:59:50 +0545

    updating view product and success add product

--- onlinestationary_store/src/services/product.service.js
+++ onlinestationary_store/src/services/product.service.js
@@ -24,3 +24,9 @@ export const addProductImage=(file,productId)=>{
     .post(`/products/image/${productId}`,formData)
     .then((response)=>response.data);
 }
+
+//get products
+export const getAllProducts=(pageNumber=0,pageSize=10,sortBy='title',sortDir='asc')=>{
+    return privateAxios.get(`/products?pageNumber=${pageNumber}&pageSize=${pageSize}&sort`,)
+
+}
